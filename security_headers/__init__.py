d1b2c8ce178c909ea0c50ccdfc9d3e97d087f3b4
R_N_A <33271130+RNA4219@users.noreply.github.com>
2025-11-01 09:40:14 +0900

    chore: remove unused network and security modules

--- security_headers/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Security headers middleware utilities."""
-
-from .middleware import SecurityHeadersConfig, SecurityHeadersMiddleware
-
-__all__ = ["SecurityHeadersConfig", "SecurityHeadersMiddleware"]
